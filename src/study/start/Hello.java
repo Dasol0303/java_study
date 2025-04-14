737022c7ebdf8c1e4635229563014ce3a2420fd1
Dasol0303 <ekthf9135@gmail.com>
2025-04-14 11:35:43 +0900

    자바 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,26 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!");
+		
+		
+		//주석
+		// 실행 (Run) : Ctrl + F11
+		
+		/* 
+		 * 여러줄 주석
+		 * 
+		 * */
+//		주석
+//		 * 단축키 : Ctrl + /
+//		 * 		  Ctrl + Shift + C
+
+		
+		
+	}
+
+}
