0357286a77ceeb238295d5a1cb533d3e7cbca360
Dasol0303 <ekthf9135@gmail.com>
2025-04-17 17:29:44 +0900

    집에서 연습

---
+++ src/study/practice/Prac_h.java
@@ -0,0 +1,154 @@
+package study.practice;
+
+
+public class Prac_h {
+
+	public static void main(String[] args) {
+		//prac01_1();
+		//prac01_2();
+		//prac01_3();
+		//prac01_4();
+		//prac01_5();
+		//prac01_678();
+		
+		//prac02_1();
+		//prac02_2();
+		//prac02_3();
+		//prac02_4();
+		//prac02_5();
+		
+		//operator01();
+		
+		
+	}
+		
+	public static void prac01_1() {
+		//오늘이 몇월 몇일인지 저장한 변수가 필요한 상태이다.
+				//1. 월에 해당하는 값을 저장할 month 라는 이름의 정수형 int 변수 선언하세요.
+				int month;
+	}
+	public static void prac01_2() {
+		//2. 일자에 해당하는 값을 저장할 day 라는 이름의 정수형 int 변수를 선언함과 동시에 1로 초기화 하세요.
+				int day = 1;
+	}
+	public static void prac01_3() {
+		//3. 위 1번 2번에서 만든 월과 일을 저장하기위해 만든 변수에 오늘 날짜에 맞게 해당하는 값을 저장하고,
+		// 아래와 같이 결과가 나타나도록 출력하세요. (띄어쓰기까지 포함하여 동일하게 출력)
+		//오늘은 8월 29일 입니다.
+		int month;
+		int day;
+		month = 4;
+		day = 17;
+		System.out.println("오늘은 " + month + "월 " + day + "일 입니다.");
+	}
+	public static void prac01_4() {
+		//4. 이름을 저장할 변수를 선언하고 본인의 이름을 저장한 후에 출력하시오. (변수명을 의미있게)
+				String name = "진다솔";
+				System.out.println(name);
+	}
+	public static void prac01_5() {
+		//5. 전화번호를 저장할 변수를 선언과 동시에 값을 초기화 하여 저장하고,
+				// 아래와 같이 결과가 나타나도록 출력하세요. (각자 입력하여 저장한 번호)
+				//전화번호는 OOO-OOOO-OOO 입니다.
+				String tel;
+				tel = "010-1234-5678";
+				System.out.println(tel);
+	}
+	public static void prac01_678() {
+		//6. 다음중 변수명으로 사용할 수 있는 것은?
+		/*
+			사용할 수 없는 경우 : 잘못된 사유를 옆에 함께 작성
+			사용할 수 있으나 잘못된 부분이 있으면 해당 사유를 함께 작성
+			a. $ystem
+				- O
+			b. channel#112
+				- X 
+			c. 7eleven
+				- X (숫자로 시작되는 문구 X)
+			d. newmember
+				- O
+			e. $MAX_NUM
+				- O
+			f. hello@com
+				- X
+			g. int
+				- X (정수타입에 사용되는 단어라서 X)
+
+			//7. 다음중 변수 초기화가 잘못 된 것은?
+			a. int b = 255;
+			b. String s = "T";
+			c. char answer = 'no';
+			d. char flag = 'F';
+
+			//8. 다음 정수형 타입들 중에서 자바가 정수형 값을 만났을때 가장 기본적(대표적)으로 인식하는 변수 타입은?
+			a. char
+			b. int
+			c. short
+			d. long
+		*/
+	}
+	public static void prac02_1() {
+		/*
+		1.
+		키보드를 가지고 있습니다. 키보드에 대한 여러 정보를 저장하고 싶은데
+		아래 데이터들에 따라서 필요한 변수들을 선언하고 값을 저장해보세요!
+
+		브랜드명 : 로지텍
+		상품명 : mx keys
+		상품가격 : 135000
+		시리얼번호 : 332L1545K
+		*/
+	}
+	}
+	public static void prac02_2() {
+		/*
+		2. 가지고 있는 모니터 정보를 저장할 필요가 생겼습니다.
+		관련 정보 저장을 위해 아래 데이터 저장을 위한 변수를 선언하고 값을 저장해보세요!
+
+		회사명 : SAMSUNG
+		모니터인치 : 24
+		모니터색상 : black
+		HDMI포트여부 : true
+		소리출력가능여부 : false
+		*/
+	}
+	}
+	public static void prac02_3() {
+		//3. 3.14 값을 가지고 있는 pi 라는 실수형 변수를 만들고 화면에 출력하세요.
+		//예시) 원주율 : 3.14
+	}
+	}
+	public static void prac02_4() {
+		/*
+		4. 세 개의 문자열 변수가 우리에게 주어졌습니다. 그런데 해당 문자열 변수는 어떤 학생의 점수를 가지고 있습니다.
+		주어진 점수들의 전체 합을 계산하고 싶습니다. 모든 점수를 더해서 총점이 얼마인지 출력하세요.
+		(*힌트 : 숫자처럼 덧셈을 하기위해서는 숫자형태로의 변환이 필요합니다.)
+
+		주어진 변수
+		String mathScore = "85";
+		String englishScore = "90";
+		String artScore = "70";
+		*/
+	}
+	}
+	public static void prac02_5() {
+		/*
+		5. 조카에게 나눗셈 계산하는 연습을 시키고 있습니다.
+		4 / 10 을 실행시켜서 결과가 0.4 가 나오는 것을 보여주려고 하는데 아래 계산 결과가 맞지 않습니다.
+		정상적으로 0.4 가 계산되서 출력되도록 만들어주세요.
+
+		int num1 = 4;
+		int num2 = 10;
+		double num3 = num1 / num2 ;
+		System.out.println( num3 );
+		*/
+	}
+	public static void operator01() {
+		
+	}
+	
+
+	
+}
+	
+
