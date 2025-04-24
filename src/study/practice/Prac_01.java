a2da5adc9c86954c9d52a64792c31b578447dc4f
Dasol0303 <ekthf9135@gmail.com>
2025-04-24 22:14:15 +0900

    집>학원

--- src/study/practice/Prac_01.java
+++ src/study/practice/Prac_01.java
@@ -1,5 +1,7 @@
 package study.practice;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 public class Prac_01 {
 
 	public static void main(String[] args) {
@@ -38,42 +40,97 @@ public class Prac_01 {
 		+ 탑승 인원 만큼, 사람들이 내릴 층(2층부터 100층까지)를 각각 입력 받습니다.
 		*/
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("탑승인원 입력 : ");
 		int person = scanner.nextInt();
 
-		
+
 		int[] personArr = new int[person];
 
+
+		//while(true) {
+		for(int i=0; i<personArr.length; i++) {
+			System.out.println("내리는 층 입력 : ");
+			int num = scanner.nextInt();
+
+			if(num>=2 && num<=100) {
+				//num = scanner.nextInt();
+				personArr[i] += num;
+				continue;
+			}
+			if(num>100) {
+				num = 0;
+				i--;
+				System.out.println("없는 층입니다. 다시 입력해주세요.");
+				continue;
+			}
+
+		}
+		//}
 		
-		while(true) {
-			for(int i=0; i<=personArr.length; i++) {
-				System.out.println("내리는 층 입력 : ");
-				int num = scanner.nextInt();
-
-				if(num>=1 && num<=100) {
-					num = scanner.nextInt();
-					personArr[i] += num;
-					continue;
-				}
-				if(num>100) {
-					num = 0;
-					System.out.println("없는 층입니다. 다시 입력해주세요.");
-					continue;
-				}
-
+		//To. 쌤...
+		//아니.. 그 이런 배열을 원하긴 했는데 이거 이렇게 찾아서 써도 되는건가요..???
+		Arrays.sort(personArr);
+        for (int i : personArr) 
+            System.out.println(i + "(내림)");
+		
+		
+		
+/*
+		System.out.println("내리는 층");
+		
+		
+		
+		int[] numArr = new int[person];
+		//가장 큰 수 하나 빼서 그 수를 numArr[0]에 넣고 그 다음으로는 가장 큰 수 기준으로 비교하고 큰 수대로 넣기?
+		//-> 식 중간에 가장 작은 수보다 큰 수가 나오면 얘를 저장하네........?
+		//가장 작은 수도 빼야하나.. 아닌 듯..?
+		
+		int j=0; //가장 큰 수 저장할 공간
+		
+		for(int i=0; i<personArr.length; i++) {
+			if(j<=personArr[i]) {
+				j=personArr[i];
 			}
 		}
-//			if (num>100) {
-//				System.out.println("없는 층입니다. 다시 입력해주세요.");
-//				continue;
-//			}
-
 		
+		numArr[0] = j;
+		
+		for(int i=0; i<personArr.length; i++) {
 			
-			
+		}
+		
+//		for(int i=0; i<personArr.length; i++) {
+//			for(int j=person-1; j>=0; j--) {
+//				
+//				if(personArr[i] > personArr[j]) {
+//					numArr[i] += personArr[i];
+//					break;
+//				}
+//				if(personArr[i] <= personArr[j]) {
+//					numArr[i] += personArr[j];
+//					break;
+//				}
+//				if(i == personArr.length-1) {
+//					
+//				}
+//				
+//			}
+//		}
+		
+		for(int i=0; i<numArr.length; i++) {
+			System.out.println(numArr[i] + ("내림"));
+		}
 
-
+//		if(personArr[i] > personArr[j]) {
+//			System.out.println(personArr[i] + "(내림)");
+//			continue;
+//		}
+//		if(personArr[i] < personArr[j]) {
+//			i--;
+//			continue;
+//		}
+*/
 		
 		
 		
