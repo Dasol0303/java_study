9e16cac0af0a85f0323968abe7308166fa892df3
Dasol0303 <ekthf9135@gmail.com>
2025-05-07 10:47:09 +0900

    연습35 클래스

---
+++ src/study/practice/practice35/Menu.java
@@ -0,0 +1,20 @@
+package study.practice.practice35;
+
+public class Menu {
+	// 필드
+	String name;
+	int price;
+	// 생성자
+	Menu() {
+		
+	}
+	
+	public Menu(String str, int i) {
+	name = str;
+	price = i;
+	}
+	
+
+	
+	
+}
