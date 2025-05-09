5ee33dbfac39305bca8b2fecbd3f33f099c26083
Dasol0303 <ekthf9135@gmail.com>
2025-05-09 14:31:18 +0900

    클래스 상속 강제타입변환

--- src/study/inherit/inherit06/Main.java
+++ src/study/inherit/inherit06/Main.java
@@ -28,6 +28,39 @@ public class Main {
 		Vehicle v4 = takeVehicle(); //-> 가능!
 		//(명시된 걸 따라가야하기 때문에 위 코드보단 여기로!!)
 		
+		//------------------------------------------------
+		
+		Vehicle vv = new Vehicle();
+		
+		Vehicle vv2 = new Bus();//타입은 Vehicle이지만 몸통은 Bus다!!
+		//vv2.start();	-> 불가!!
+		//vv2.run();	-> 가능!!
+		
+		Bus bb = new Bus();
+		bb.run();
+		bb.start();
+		
+		Bus bb2 = (Bus)vv2; //강제 형변환
+		bb2.start();//강제 형변환으로 호출 가능해짐!!
+		
+//		Bus bb3 = (Bus)vv; //오류!! 몸체가 순수 Vehicle이라서 안됨
+//		bb3.start();
+		
+		//-----------------------------------
+		Vehicle vv4 = takeVehicle();
+
+		if(vv4 instanceof Bus) {
+			Bus bbb = (Bus)vv4;
+			System.out.println("Bus가 들어있음");
+		}
+
+		if(vv4 instanceof Taxi) {
+			Taxi bbb = (Taxi)vv4;
+			System.out.println("Taxi가 들어있음");
+		}
+		
+		//Bus bbb = (Bus)vv4;
+		//bbb.start();
 		
 	}
 	
@@ -47,8 +80,16 @@ public class Main {
 		
 		
 		//return new Vehicle();//-> 가능!
-		return new Bus();
+		//return new Bus();
 		//return new Taxi();//-> 가능!
+		
+		int n = (int)(Math.random()*2) + 1;
+		
+		if(n==1)
+			return new Bus();
+		else
+			return new Taxi();
+		
 	}
 	
 
