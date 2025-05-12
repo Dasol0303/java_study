8e5244a8a1767e89e7ffbbee4b59bd19f733ae08
Dasol0303 <ekthf9135@gmail.com>
2025-05-12 15:45:51 +0900

    연습43 푸드트럭 리펙토링

---
+++ src/study/practice/practice43/SalesInfo.java
@@ -0,0 +1,11 @@
+package study.practice.practice43;
+
+public class SalesInfo {
+	//일자별 매출정보
+	String salesDate; // "20250512" 20221211
+	int totalSales; //매출
+	int lossSales; //손해금액
+	int incom; //최종 수익
+	
+	
+}
